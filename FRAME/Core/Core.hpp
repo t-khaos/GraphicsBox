9f3ac3009a74a2534cd8f4faa47c4801a384cb00
Justian <just.tzh@outlook.com>
2023-12-28 09:37:56 +0800

    Update Core.hpp

--- FRAME/Core/Core.hpp
+++ FRAME/Core/Core.hpp
@@ -1,25 +1,7 @@
 #pragma once
 
 #ifdef FRAME_EXPORT
-#define FRAME_API __declspec(dllexport)
+    #define FRAME_API __declspec(dllexport)
 #else
     #define FRAME_API __declspec(dllimport)
 #endif // FRAME_API
-
-
-namespace FRAME
-{
-    template<typename T>
-    using Unique = std::unique_ptr<T>;
-    template<typename T, typename... Args>
-    constexpr Unique<T> CreateUnique(Args&&... args) {
-        return std::make_unique<T>(std::forward<Args>(args)...);
-    }
-
-    template<typename T>
-    using Shared = std::shared_ptr<T>;
-    template<typename T, typename... Args>
-    constexpr Shared<T> CreateShared(Args&&... args) {
-        return std::make_shared<T>(std::forward<Args>(args)...);
-    }
-}
